97a633ff882c913f2edde0df2f16c8fddc785eaf
sepehrow <66973440+sepehrow@users.noreply.github.com>
2022-01-09 14:23:43 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
+<?php
 
+$content = file_get_contents("put://input");
+
+
+$update = json_decode($content, true);
+
+$chat_id = $update["message"] ['chat'] ['id'];
+$text = $update["message"]['text'];
+
+$url = 'https://api.telegram.org/bot5017943929:AAFYkEqdueIIKVOn_wZoMAGpXduRA6hRSCs/sendMessage?chat_id='.$chat_id.'&text'.$content;
+
+file_get_contents($url) ;
+
+?>
